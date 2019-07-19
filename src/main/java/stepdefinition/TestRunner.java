16bdb65318fe88166eba529d806eb2d2570f590e
pradeerithi <pradeerithi27@@gmail.com>
2019-07-19 13:20:29 +0530

    cucumber class

--- src/main/java/stepdefinition/TestRunner.java
+++ src/main/java/stepdefinition/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\java\\Features", tags = {}, strict = false, dryRun = false, glue = "stepdefinition", monochrome = true, plugin = "html:target")
+@CucumberOptions(features = "src\\test\\java\\Features\\AddCustomer.feature", tags = {}, strict = false, dryRun = false, glue = "stepdefinition", monochrome = true, plugin = {"html:target","json:target/cucumber.json"})
 public class TestRunner {
 
 }
